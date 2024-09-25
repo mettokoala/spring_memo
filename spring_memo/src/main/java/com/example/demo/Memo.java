7e8f0013d5614dc8a1c846e804885604f34b5b8d
yamakoala <52974545+mettokoala@users.noreply.github.com>
2024-09-26 08:21:03 +0900

    Repository、Entityの作成

--- spring_memo/src/main/java/com/example/demo/Memo.java
+++ spring_memo/src/main/java/com/example/demo/Memo.java
@@ -1,20 +1,22 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Memo {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String title;
 	private String content;
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
 	
 }
--- spring_memo/src/main/java/com/example/demo/service/MemoService.java
+++ spring_memo/src/main/java/com/example/demo/service/MemoService.java
@@ -1,27 +1,22 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Memo;
+import com.example.demo.repository.MemoRepository;
 
 @Service
 public class MemoService {
+	private final MemoRepository memoRepository;
+	
+	private MemoService(MemoRepository memoRepository) {
+		this.memoRepository = memoRepository;
+	}
+	
 	public List<Memo> getMemos() {
-		List<Memo> memos = new ArrayList<>();
-		
-		Memo memo1 = new Memo();
-		memo1.setTitle("タイトル1");
-		memo1.setContent("内容1");
-		Memo memo2 = new Memo();
-		memo2.setTitle("タイトル2");
-		memo2.setContent("内容2");
-		memos.add(memo1);
-		memos.add(memo2);
-		
-		return memos;
+		return memoRepository.findAll();
 	}
 
 }
